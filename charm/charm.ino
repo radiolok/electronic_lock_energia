15daed13c03cc19d48c39d0d8a7d521dc951b55c
radiolok <radiolok@yandex.ru>
2015-05-18 13:16:17 +0300

    MOD: add serial output debug and input pin reading - I forgot to read pin, when wrote initial code at 3.00 am. This version is work

--- charm/charm.ino
+++ charm/charm.ino
@@ -4,6 +4,8 @@
 #define ADDRESS_LOCAL    0x02
 #define ADDRESS_REMOTE   0x03
 
+enum {IDLE=0x01, ACTIVE, ALERT};
+
 unsigned char txData[60] = { 
   "\0" };
 
@@ -18,34 +20,38 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
   pinMode(redPin, OUTPUT);
+  Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(setButton, INPUT_PULLUP);     
   pinMode(releaseButton, INPUT_PULLUP);  
   Radio.begin(ADDRESS_LOCAL, CHANNEL_1, POWER_MAX);
+  Serial.println("RF start");
   txData[0] = ADDRESS_LOCAL;//set zer byte as our address
 }
 
 void loop(){
   //check set button
-  if (setButton == HIGH){
-    digitalWrite(ledPin, HIGH);
-    txData[1] = 0x01;//Set command
+  if (digitalRead(setButton) == LOW){
+    Serial.println("Active mode");
+    digitalWrite(redPin, HIGH);
+    txData[1] = ACTIVE;//Set command
     txData[2] = 0x00;
     Radio.transmit(ADDRESS_REMOTE, (unsigned char*)&txData, 2);
   }
   else{
-    digitalWrite(ledPin, LOW);
+    digitalWrite(redPin, LOW);
   }
  //check release button
-  if (releaseButton == HIGH){
-    digitalWrite(redPin, HIGH);
-    txData[1] = 0x02;//Set command
+  if (digitalRead(releaseButton) == LOW){
+    Serial.println("IDLE mode");
+    digitalWrite(ledPin, HIGH);
+    txData[1] = IDLE;//Set command
     txData[2] = 0x00;
     //send packet
     Radio.transmit(ADDRESS_REMOTE, (unsigned char*)&txData, 2);
   }
   else{
-    digitalWrite(redPin, LOW);
+    digitalWrite(ledPin, LOW);
   }
   delay(200);
 }

b1a0649119db109a66735ef097f0ee0c8115133c
radiolok <radiolok@yandex.ru>
2015-05-18 16:12:43 +0300

    MOD: comment

--- charm/charm.ino
+++ charm/charm.ino
@@ -1,37 +1,41 @@
 #include <SPI.h>
 #include <AIR430BoostETSI.h>
 
+//define addresses. 
 #define ADDRESS_LOCAL    0x02
 #define ADDRESS_REMOTE   0x03
 
-enum {IDLE=0x01, ACTIVE, ALERT};
+//FSM states. And commands
+enum {
+  IDLE=0x01, ACTIVE, ALERT};
 
 unsigned char txData[60] = { 
   "\0" };
 
 // constants won't change. They're used here to 
 // set pin numbers:
-const int setButton = PUSH1;     // the number of the pushbutton pin
-const int releaseButton = PUSH2;
-const int ledPin =  GREEN_LED;      // the number of the LED pin
+const int setButton = PUSH1; //set to active mode
+const int releaseButton = PUSH2;//set to idle mode
+const int ledPin =  GREEN_LED; 
 const int redPin = RED_LED;
 
 void setup() {
-  // initialize the LED pin as an output:
+  //define pinouts and add serial outputr
   pinMode(ledPin, OUTPUT);      
   pinMode(redPin, OUTPUT);
   Serial.begin(9600);
-  // initialize the pushbutton pin as an input:
+
   pinMode(setButton, INPUT_PULLUP);     
   pinMode(releaseButton, INPUT_PULLUP);  
   Radio.begin(ADDRESS_LOCAL, CHANNEL_1, POWER_MAX);
   Serial.println("RF start");
-  txData[0] = ADDRESS_LOCAL;//set zer byte as our address
+  txData[0] = ADDRESS_LOCAL;//set zer0 byte as our address
 }
 
 void loop(){
   //check set button
   if (digitalRead(setButton) == LOW){
+    //if button is pressed, send command
     Serial.println("Active mode");
     digitalWrite(redPin, HIGH);
     txData[1] = ACTIVE;//Set command
@@ -41,7 +45,7 @@ void loop(){
   else{
     digitalWrite(redPin, LOW);
   }
- //check release button
+  //check release button
   if (digitalRead(releaseButton) == LOW){
     Serial.println("IDLE mode");
     digitalWrite(ledPin, HIGH);
@@ -53,5 +57,7 @@ void loop(){
   else{
     digitalWrite(ledPin, LOW);
   }
-  delay(200);
+  sleepSeconds(1);//Low-power mode delay
+  delay(50);//to Clock stabilization
 }
+
